bf447e9f1b2713b3e19f31ffc452ddbccd19a58c
Alex <scorpionajerx@gmail.com>
2017-12-07 15:37:57 +0400

    Change data type to dict

--- botan.py
+++ botan.py
@@ -20,7 +20,7 @@ def track(token, uid, message, name='Message'):
         r = requests.post(
             TRACK_URL,
             params={"token": token, "uid": uid, "name": name},
-            data=json.dumps(make_json(message)),
+            data=make_json(message),
             headers={'Content-type': 'application/json'},
         )
         return r.json()
